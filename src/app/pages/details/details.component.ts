8fe6e936aa096ef073cefc1b501654cf44b04e54
brabdalla <brabdalla@gmail.com>
2023-10-11 11:05:36 -0400

    Primeiro Commit pokedex curso Angular

---
+++ src/app/pages/details/details.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit{
+
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService,
+  ){}
+
+  ngOnInit(): void {
+    this.getPokemon();
+      
+  }
+
+  public getPokemon(){
+
+    const id = this.activeRoute.snapshot.params['id']
+    const pokemon = this.pokeApiService.ApiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.ApiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res =>{
+        this.pokemon = res;
+        this.isLoading = true;
+
+      },
+      error =>{
+
+        this.apiError = true;
+
+      }
+    )
+
+  }
+
+}
